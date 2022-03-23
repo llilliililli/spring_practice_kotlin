ad3c59a7e948ec112f5058cd01ab6f3056f526da
nego_home <wnsrn403@naver.com>
2022-03-23 23:39:50 +0900

    Initial commit

---
+++ src/main/kotlin/com/llilliililli/practice_kotlin/PracticeKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.llilliililli.practice_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/llilliililli/practice_kotlin/PracticeKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.llilliililli.practice_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PracticeKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
